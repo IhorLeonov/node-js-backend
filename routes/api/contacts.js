04f48b0e8f62583143b5045f50e4010cf6993a1d
Ihor Leonov <ihorleonow@gmail.com>
2023-05-25 17:37:22 +0300

    add schema

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,20 @@
 const express = require('express');
-
 const ctrl = require('../../controllers/contacts');
 
-const { validateBody } = require('../../middlewares');
+// const { validateBody } = require('../../middlewares');
 
-const schemas = require('../../schemas/contacts');
+// const schemas = require('../../schemas/contacts');
 
 const router = express.Router();
 
 router.get('/', ctrl.getAll);
 
-router.get('/:contactId', ctrl.getById);
+// router.get('/:contactId', ctrl.getById);
 
-router.post('/', validateBody(schemas.addSchema), ctrl.add);
+// router.post('/', validateBody(schemas.addSchema), ctrl.add);
 
-router.put('/:contactId', validateBody(schemas.addSchema), ctrl.updateById);
+// router.put('/:contactId', validateBody(schemas.addSchema), ctrl.updateById);
 
-router.delete('/:contactId', ctrl.deleteById);
+// router.delete('/:contactId', ctrl.deleteById);
 
 module.exports = router;
