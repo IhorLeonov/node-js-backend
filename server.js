36f0926ce4be78fe1fbabc3b1690d9293e017a2c
Ihor Leonov <ihorleonow@gmail.com>
2023-05-27 10:33:14 +0300

    remove excess

--- server.js
+++ server.js
@@ -13,10 +13,3 @@ mongoose
         console.log(error.message);
         process.exit(1);
     });
-
-async function maine() {
-    await mongoose.connect(process.env.DB_HOST);
-
-    console.log('Database connection successful');
-}
-maine().catch(console.error);
